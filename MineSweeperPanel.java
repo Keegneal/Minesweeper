e3a22cefe0dffee95c409f2134948bad52a2b8f7
Keegneal <92490783+Keegneal@users.noreply.github.com>
2023-10-07 19:14:39 -0700

    Add files via upload
    Updated game logic to show winner correctly

--- MineSweeperPanel.java
+++ MineSweeperPanel.java
@@ -14,7 +14,7 @@ public class MineSweeperPanel extends JPanel{
     public MineSweeperPanel(){
 
         setLayout(new GridLayout(row, col));
-        unrevealedCells = row * col - 5;
+        unrevealedCells = row * col -9;
         
         board = new JButton[row][col];
         mines = new boolean[row][col];
@@ -67,39 +67,39 @@ public class MineSweeperPanel extends JPanel{
             numMines--;
         }   
     }
-    public void revealCell(int row, int col){
-        int mineCount=0;
-                 if(mines[row][col]){
-                board[row][col].setText("BOMB");
-                gameOver();
-             }
-             else{
-               
-                mineCount = surroundMines(row, col);
-                board[row][col].setText(Integer.toString(mineCount));
-                unrevealedCells--;
-
-                // Check if the player has won
-                if (unrevealedCells == 0) {
-                    gameWon();
-                }
-                
-              }
-               if (mineCount == 0) {
+    public void revealCell(int row, int col) {
+        if (!board[row][col].getText().equals("")) {
+            return; // Cell already revealed
+        }
+    
+        if (mines[row][col]) {
+            board[row][col].setText("BOMB");
+            gameOver();
+        } else {
+            int mineCount = surroundMines(row, col);
+            board[row][col].setText(Integer.toString(mineCount));
+            unrevealedCells--;
+            System.out.println("Unrevealed Cells: " + unrevealedCells);
+            // Check if the player has won
+            if (unrevealedCells == 0) {
+                gameWon();
+            }
+    
+            if (mineCount == 0) {
+                // Recursively reveal neighboring cells with mineCount == 0
                 for (int i = -1; i <= 1; i++) {
                     for (int j = -1; j <= 1; j++) {
                         int newRow = row + i;
                         int newCol = col + j;
                         if (newRow >= 0 && newRow < this.row && newCol >= 0 && newCol < this.col) {
-                            if (board[newRow][newCol].getText().equals("")) {
-                                revealCell(newRow, newCol);
-                            }
+                            revealCell(newRow, newCol);
                         }
                     }
                 }
             }
-            
         }
+    }
+    
 
         public int surroundMines(int row, int col) {
             int mineCount = 0;
@@ -131,8 +131,8 @@ public class MineSweeperPanel extends JPanel{
      public void gameWon() {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (mines[i][j]) {
-                    board[i][j].setText(" "); 
+                if (!mines[i][j] && board[i][j].getText().isEmpty()) {
+                    return; // Player has not won yet
                 }
             }
         }
